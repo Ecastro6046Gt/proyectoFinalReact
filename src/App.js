b889feb7df2b003395336622f412ed88046d5313
Ernesto Castro <ecastro6046@gmail.com>
2024-11-15 11:41:00 -0600

    se cargan archivos de proyecto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './componentes/Home';
+import { Articulos } from './componentes/Articulo';
+import NewTask from './componentes/NewTask';
+import TaskDetail from './componentes/TaskDetail';
+import { RouterPrincipal } from './Routers/RouterPrincipal';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container mt-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/new-task" element={<NewTask />} />
+          <Route path="/task/:id" element={<TaskDetail />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
